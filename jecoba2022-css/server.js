ee0898c10ac175362d3f811ab7adadd808d5a2b6
kastro-team <jiungdev@gmail.com>
2022-08-16 14:45:14 +0900

    01, express app use, set path

--- jecoba2022-css/server.js
+++ jecoba2022-css/server.js
@@ -2,4 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(port, () => console.log(`Dolphin app listening on port ${port}!`))
+app.get('/', function(req,res){
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
+app.use(express.static('public'))
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
