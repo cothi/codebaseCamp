cb05834811470ae7107fe83aa1b3407aa18e0420
kastro-team <jiungdev@gmail.com>
2022-08-17 15:54:34 +0900

    02, set option

--- jecoba2022-css/server.js
+++ jecoba2022-css/server.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var liveServer = require("live-server");
 
-app.get('/', function(req,res){
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-
-app.use(express.static('public'))
-
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+var params = {
+	port: 8181, // Set the server port. Defaults to 8080.
+	host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+	root: "/public", // Set root directory that's being served. Defaults to cwd.
+	open: false, // When false, it won't load your browser by default.
+	ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+	file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+	wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+	mount: [['/components', './node_modules']], // Mount a directory to a route.
+	logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+	middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+liveServer.start(params);
